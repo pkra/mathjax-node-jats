8428995b413b457c94ccf84b6acfe8ea6fd99fe4
Peter Krautzberger <p.krautzberger@gmail.com>
2015-12-29 21:53:02 +0100

    add postinstall script to automate img.js integration; fixes #2

---
+++ postInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/mathjax/MathJax-third-party-extensions/master/img/unpacked/img.js -O node_modules/MathJax/unpacked/extensions/TeX/img.js
+sed -i 's/\[Contrib\]\/img\/unpacked/\[MathJax\]\/extensions\/TeX/g' ./node_modules/MathJax/unpacked/extensions/TeX/img.js
+sed -i 's/"autoload-all.js"/"autoload-all.js","img.js"/g' ./node_modules/mathjax-node/lib/mj-single.js
